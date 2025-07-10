6e367eb161343ae6e3d747f7ca6679fc9a49711a
Angelicious24 <angelicameyer98@gmail.com>
2025-07-10 14:56:13 +0200

    First Commit
    23/06/2025

---
+++ lists.py
@@ -0,0 +1,17 @@
+
+fruits = ["apple", "banana" , "grape"]
+
+fruits.append("cherry")
+print(fruits)
+
+fruits.insert(1, "avo")
+print(fruits)
+
+fruits.remove("cherry")
+print(fruits)
+
+fruits.sort()
+print(fruits)
+
+fruits.sort(reverse=True)
+print(fruits)
